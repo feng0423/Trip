43337628bba1a6d591c503bd5f769be1ad6dda51
Zoe1an <zhengyanhua18@163.com>
2018-12-16 15:05:34 +0800

    问达人模块的提问

--- admin/src/main/java/cn/wolfcode/trip/admin/controller/NewsController.java
+++ admin/src/main/java/cn/wolfcode/trip/admin/controller/NewsController.java
@@ -27,10 +27,15 @@ public class NewsController {
     @RequestMapping("/saveOrUpdate")
     @ResponseBody
     public JsonResult saveOrUpdate(News news, MultipartFile file) {
+
+        String url = UploadUtil.uploadQiniyun(file);
         //判断是否上传文件
         if(file!=null && file.getSize()>0){
-            String url = UploadUtil.upload(file, UploadUtil.Qi_PATH+"/upload");
-            news.setCoverUrl(url);
+
+
+            news.setCoverUrl(UploadUtil.Qi_PATH+url);
+          //  String url = UploadUtil.upload(file, UploadUtil.Qi_PATH+"/upload");
+            //news.setCoverUrl(url);
         }
         newsService.saveOrUpdate(news);
         return new JsonResult();

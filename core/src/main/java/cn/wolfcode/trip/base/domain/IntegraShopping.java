e75807c121daa8a44a4adf5f305b3c5babd9bd5f
zhuzhu <2484382677@qq.com>
2018-12-17 09:40:59 +0800

    朱远庆项目更新2

---
+++ core/src/main/java/cn/wolfcode/trip/base/domain/IntegraShopping.java
@@ -0,0 +1,19 @@
+package cn.wolfcode.trip.base.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class IntegraShopping  extends BaseDomain{
+
+    private String productName;//商品名称
+
+    private String productDescription;//商品描述
+
+    private String coverUrl;//商城图片
+
+    //private Integer sign_id;
+
+}
